3157488714633f49246bf2003b9a1b4b70dd2b74
Piotr Pikosz <piotr.pikoszofficial@gmail.com>
2024-05-30 18:44:20 +0200

    update 30.05 migration dataBase

--- src/main/resources/db/migration/V1_4__init_Visit.sql
+++ src/main/resources/db/migration/V1_4__init_Visit.sql
@@ -1,14 +1,14 @@
 CREATE TABLE visit
 (
-    visit_id            SERIAL                      NOT NULL,
-    availableDate_id    TIMESTAMP WITH TIME ZONE    NOT NULL,
-    patient_id          INT                         NOT NULL,
+    visit_id            SERIAL      NOT NULL,
+    availableDate_id    INT         NOT NULL,
+    patient_id          INT         NOT NULL,
     PRIMARY KEY (visit_id),
     CONSTRAINT fk_visit_patient
         FOREIGN KEY (patient_id)
             REFERENCES patient (patient_id),
     CONSTRAINT fk_visit_availableDate
             FOREIGN KEY (availableDate_id)
-                REFERENCES availableDate (availableDate_id)
+                REFERENCES available_Date (availableDate_id)
 
 );
