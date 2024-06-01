cdbc0a467f68ccb447e4e1621f50b846c03c5783
Piotr Pikosz <piotr.pikoszofficial@gmail.com>
2024-06-01 22:15:19 +0200

    update 01.06 database preparation

---
+++ src/main/java/my/project/business/dao/PatientDAO.java
@@ -0,0 +1,13 @@
+package my.project.business.dao;
+
+import my.project.domain.Patient;
+
+import java.util.Optional;
+
+public interface PatientDAO {
+
+    Optional<Patient> findByPesel(String pesel);
+    Optional<Patient> findByPhone(String phone);
+    Optional<Patient> findByEmail(String email);
+
+}
